09bb98b209618fa49169b7ba275e978acc787513
Robertarezza <roberta.rezza@gmail.com>
2024-06-28 16:11:47 +0200

    add: milestone 5

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,19 @@
 <script>
 //importiamo axios per la chiamata api
 import axios from "axios";
+//importiamo il component ProjectCard
+import ProjectCard from "./ProjectsCard.vue";
+
+
 export default {
+    components: {
+        ProjectCard,
+    },
+
   data() {
     return {
       //defiamo projects come array vuoto
       projects: [],
-      //inseriamo il percorso per il recupero delle immagini dal nostro strorage di Laravel
-      imageUrl: 'http://127.0.0.1:8000/storage',
     };
   },
   //si fa partire la chiamata all'avvio della pagina
@@ -17,7 +23,7 @@ export default {
       //console.log(resp)
 
       this.projects = resp.data.results;
-       console.log(this.projects)
+      console.log(this.projects);
     });
   },
 };
@@ -25,26 +31,10 @@ export default {
 
 <template>
   <div class="container mt-5 mb-5">
-  <h1 class="text-center mb-3">I nostro Progetti</h1>
+    <h1 class="text-center mb-3">I nostro Progetti</h1>
     <div class="row row-cols-4 g-3">
       <div class="col" v-for="project in projects">
-        <div class="card h-100">
-          <img :src="project.cover_image ? `${imageUrl}/${project.cover_image}` : `https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg`" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">{{project.title}}</h5>
-            <p class="card-text">
-              Stato di lavorazione: {{ project.status }}
-            </p>
-            <p>Tecnologie Usate:</p>
-            <p class="card-text" v-for="technology in project.technologies">
-               {{ technology.name }} 
-            </p>
-            <p class="card-text">
-              Tipologia di Progetto:  {{ project.type ? project.type.name : "Nessuna tipologia" }}
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
+       <ProjectCard :project="project" />
       </div>
     </div>
   </div>
---
+++ src/components/ProjectsCard.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    props: {
+        project: Object, 
+    },
+  data() {
+    return {
+      //inseriamo il percorso per il recupero delle immagini dal nostro strorage di Laravel
+      imageUrl: "http://127.0.0.1:8000/storage",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="card h-100">
+          <img
+            :src="
+              project.cover_image
+                ? `${imageUrl}/${project.cover_image}`
+                : `https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg`
+            "
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <p class="card-text">Stato di lavorazione: {{ project.status }}</p>
+            <p>Tecnologie Usate:</p>
+            <p class="card-text" v-for="technology in project.technologies">
+              {{ technology.name }}
+            </p>
+            <p class="card-text">
+              Tipologia di Progetto:
+              {{ project.type ? project.type.name : "Nessuna tipologia" }}
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+</template>
+
+<style scoped lang="scss"></style>
