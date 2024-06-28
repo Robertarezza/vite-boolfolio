15b975e62e13ea8725c6cf8404b8b8d297b030d8
Robertarezza <roberta.rezza@gmail.com>
2024-06-28 15:03:55 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script >
+import AppMain from './components/AppMain.vue'
+
+export default {
+  components: {
+    AppMain, 
+  },
+  data() {
+    return {};
+  },
+};
+
+</script>
+
+<template>
+  <AppMain />
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+    data() {
+        return {
+            
+        }
+    },
+}
+
+</script>
+
+<template>
+    <div>
+        <h1>funzionerà?</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+</style>
